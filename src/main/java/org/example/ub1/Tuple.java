051b811bd1d51b63f36e84c0d00ed4895547d9ca
saveligulas <saveli.gulas@gmail.com>
2024-03-25 10:20:47 +0100

    .

---
+++ src/main/java/org/example/ub1/Tuple.java
@@ -0,0 +1,35 @@
+package org.example.ub1;
+
+import org.jetbrains.annotations.Nullable;
+
+public class Tuple<A, B> {
+    @Nullable
+    private A _first;
+    @Nullable
+    private B _second;
+
+    public Tuple() {
+        this(null, null);
+    }
+
+    public Tuple(A a, B b) {
+        _first = a;
+        _second = b;
+    }
+
+    public void setFirst(A a) {
+        _first = a;
+    }
+
+    public void setSecond(B b) {
+        _second = b;
+    }
+
+    public A getFirst() {
+        return _first;
+    }
+
+    public B getSecond() {
+        return _second;
+    }
+}
