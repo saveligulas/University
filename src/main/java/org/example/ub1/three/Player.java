5c81f4fc455c70d0386326cc414064804934c6b4
saveligulas <saveli.gulas@gmail.com>
2024-03-25 10:20:29 +0100

    .

--- src/main/java/org/example/ub1/three/Player.java
+++ src/main/java/org/example/ub1/three/Player.java
@@ -1,10 +1,14 @@
 package org.example.ub1.three;
 
+import org.example.ub1.three.container.BackPack;
+import org.example.ub1.three.container.ItemContainer;
+
 public class Player {
     private String _name;
     private double _health;
     private Outfit _outfit;
-    private BackPack _backPack;
+    private ItemContainer _container;
+    private Item _equippedItem;
 
     public Player() {
 
@@ -13,4 +17,22 @@ public class Player {
     public Player(String name) {
 
     }
+
+    public Player(String name, Outfit outfit) {
+
+    }
+
+    public Player(String name, Outfit outfit, ItemContainer itemContainer) {
+
+    }
+
+    public Player(String name, double health, Outfit outfit, ItemContainer itemContainer) {
+        _name = name;
+        _health = health;
+        _outfit = outfit;
+        _container = itemContainer;
+        _equippedItem = null;
+    }
+
+
 }
