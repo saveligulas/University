cfffea9e69a22ed9fdc5f7895c283d3954a63d16
Saveli <saveli.gulas@gmail.com>
2024-04-03 00:38:03 +0200

    .

--- src/main/java/org/example/ub1/Point.java
+++ src/main/java/org/example/ub1/Point.java
@@ -5,11 +5,12 @@ import lombok.Data;
 import lombok.Getter;
 
 public class Point {
-
+    //region <Fields>
     private int _xCoordinate;
     private int _yCoordinate;
+    //endregion
 
-
+    //region <Constructors>
     public Point(int x, int y) {
         _xCoordinate = x;
         _yCoordinate = y;
@@ -19,12 +20,14 @@ public class Point {
         _xCoordinate = p.getX();
         _yCoordinate = p.getY();
     }
+    //endregion
 
     public void addVector(Point point) {
         _xCoordinate += point.getX();
         _yCoordinate += point.getY();
     }
 
+    //region <Static Methods>
     public static Point addVector(Point point, Point vector) {
         return new Point(point.getX() + vector.getX(), point.getY() + vector.getY());
     }
@@ -32,7 +35,9 @@ public class Point {
     public static double distance(Point a, Point b) {
         return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
     }
+    //endregion
 
+    //region <Getters>
     public int getX() {
         return _xCoordinate;
     }
@@ -40,6 +45,7 @@ public class Point {
     public int getY() {
         return _yCoordinate;
     }
+    //endregion
 
     @Override
     public boolean equals(Object obj) {
