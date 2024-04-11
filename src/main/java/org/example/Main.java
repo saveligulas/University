a28cf2d493e88e4345c040f670abc50e123fc400
saveligulas <saveli.gulas@gmail.com>
2024-04-11 14:45:32 +0200

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.coll.MyCollection;
+import org.example.stack.StackCalculator;
 import org.example.ub1.rect.Point;
 import org.example.ub1.three.app.Gender;
 import org.example.ub2.NameGenerator;
@@ -14,6 +15,12 @@ import org.example.ub3.two.Person;
 
 public class Main {
     public static void main(String[] args) {
+        PrimeToolV2 tool = new PrimeToolV2();
+        System.out.println("With saving");
+        System.out.println(tool.expandPrimesTime(1000000) + "ms");
+        System.out.println(tool.expandPrimesTime(2000000) + "ms");
+        System.out.println(tool.expandPrimesTime(3000000) + "ms");
+        System.out.println(tool.getPrimes(2, 1000000).get(0));
 
 //        NameGenerator gen = new NameGenerator(5);
 //        int count = 0;
@@ -32,12 +39,6 @@ public class Main {
 //        TrainJob job = new TrainJob(ProductType.IRON, 400);
 //        trolley.moveTo(new Point(99, 99), job);
 //        System.out.println();
-
-        MarriageBureau b = new MarriageBureau();
-        Person male = new Person(Gender.MALE);
-        Person female = new Person(Gender.FEMALE);
-        b.marry(male, female, new MyCollection<>(), new MyCollection<>());
-        b.divorce(male, "Reason..");
     }
 
 
