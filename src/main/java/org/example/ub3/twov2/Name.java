19c006cf02d2274b3ad77c02a65e77959aee4c3f
Saveli <saveli.gulas@gmail.com>
2024-04-16 19:43:45 +0200

    .

--- src/main/java/org/example/ub3/twov2/Name.java
+++ src/main/java/org/example/ub3/twov2/Name.java
@@ -3,4 +3,7 @@ package org.example.ub3.twov2;
 import org.example.coll.MyCollection;
 
 public record Name(String first, MyCollection<String> middle, String last) {
+    public static Name updateLastName(String newLastName, Name name) {
+        return new Name(name.first, name.middle, newLastName);
+    }
 }
