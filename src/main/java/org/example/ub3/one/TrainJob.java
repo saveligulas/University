2411aab3dae5534dabb6617ab9d95e9205feb788
saveligulas <saveli.gulas@gmail.com>
2024-04-17 15:10:50 +0200

    .

--- src/main/java/org/example/ub3/one/TrainJob.java
+++ src/main/java/org/example/ub3/one/TrainJob.java
@@ -1,6 +1,8 @@
 package org.example.ub3.one;
 
+import org.example.coll.MyCollection;
+import org.example.ub3.one.pro.Product;
 import org.example.ub3.one.pro.ProductType;
 
-public record TrainJob(ProductType type, int target) {
+public record TrainJob(MyCollection<ProductType> types, int target) {
 }
