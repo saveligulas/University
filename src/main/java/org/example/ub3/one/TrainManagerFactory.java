8174057e3c191ed7aadddae8ab647ea1fecd361e
saveligulas <saveli.gulas@gmail.com>
2024-04-17 15:10:59 +0200

    .

--- src/main/java/org/example/ub3/one/TrainManagerFactory.java
+++ src/main/java/org/example/ub3/one/TrainManagerFactory.java
@@ -1,5 +1,6 @@
 package org.example.ub3.one;
 
+import org.example.coll.MyBidirectionalDictionary;
 import org.example.coll.MyCollection;
 import org.example.coll.MyDictionary;
 import org.example.ub1.rect.Point;
@@ -10,14 +11,14 @@ import org.example.ub3.one.pro.ProductType;
 import java.util.Random;
 
 public class TrainManagerFactory {
-    public TrainManager createTrainManager(int height, int width, int trolleyCount, int hubCount, int productAmount) {
+    public static TrainManager createTrainManager(int height, int width, int trolleyCount, int hubCount, int productAmount) {
         TrainNetwork network = new TrainNetwork(height, width);
         MyCollection<Trolley> trolleys = new MyCollection<>();
-        MyDictionary<Point, TrainManager.TrainHub> pointHubs = new MyDictionary<>();
+        MyBidirectionalDictionary<Point, TrainManager.TrainHub> pointHubs = new MyBidirectionalDictionary<>();
         Random rand = new Random();
 
         for (int i = 0; i < trolleyCount; i++) {
-            trolleys.add(new Trolley(network, new Point(0, 0), (int) Math.ceil(productAmount / 10)));
+            trolleys.add(new Trolley(network, new Point(0, 0), (int) Math.ceil((double) productAmount / 10)));
         }
 
         for (int i = 0; i < hubCount; i++) {
@@ -34,6 +35,6 @@ public class TrainManagerFactory {
                             pointHubs.getKeys().get(rand.nextInt(pointHubs.getKeys().size()))));
         }
 
-        return null;
+        return new TrainManager(network, trolleys, pointHubs);
     }
 }
