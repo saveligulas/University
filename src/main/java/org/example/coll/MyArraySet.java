b68a7fe9f80e834f5dcc67fa11e1d1c9aa16491f
saveligulas <saveli.gulas@gmail.com>
2024-04-17 15:10:37 +0200

    .

---
+++ src/main/java/org/example/coll/MyArraySet.java
@@ -0,0 +1,9 @@
+package org.example.coll;
+
+public class MyArraySet<T> extends MyCollection<T> {
+    public void add(T element) {
+        if (!this.contains(element)) {
+            
+        }
+    }
+}
