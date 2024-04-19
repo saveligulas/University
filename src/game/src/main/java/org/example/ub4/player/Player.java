544285853184c1b476b62ba596e3af46904b6d90
Saveli <saveli.gulas@gmail.com>
2024-04-20 01:18:13 +0200

    added TileMapFactory and ideas

--- src/game/src/main/java/org/example/ub4/player/Player.java
+++ src/game/src/main/java/org/example/ub4/player/Player.java
@@ -1,6 +1,9 @@
 package org.example.ub4.player;
 
 import org.example.coll.MyCollection;
+import org.example.ub4.excep.InteractionTileIsFullException;
+import org.example.ub4.tile.InteractionTile;
+import org.example.ub4.tile.Tile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,10 +12,25 @@ public class Player {
     private final String _username;
     private final List<Consumable> _consumables;
     private final List<String> _answers;
+    private InteractionTile _interactionTile;
 
     public Player(String username) {
         _username = username;
         _consumables = new ArrayList<>();
         _answers = new ArrayList<>();
+    }
+
+    public boolean setTile(InteractionTile tile) {
+        try {
+            _interactionTile.addPlayerToTile(this);
+        } catch (InteractionTileIsFullException e) {
+            return false;
+        }
+        _interactionTile = tile;
+        return true;
+    }
+
+    public InteractionTile getTile() {
+        return _interactionTile;
     }
 }
---
+++ src/game/src/main/java/org/example/ub4/tile/TileMapFactory.java
@@ -0,0 +1,11 @@
+package org.example.ub4.tile;
+
+public class TileMapFactory {
+    /*
+    Ideas:
+    - Create a map of booleans first in a 2d array in which i define where the tiles will be
+    - Check that the created map is possible, so no isolated tiles or tiles that are connected only diagonally
+    - Read that map and check if there are places to place rooms, doors, interactions etc.
+    - Then create the tile and id them correctly, then connect them in the right order
+     */
+}
