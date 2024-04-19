f4116b6bd34e5bc92bd4ed1ffb26d6f4c158d990
Saveli <saveli.gulas@gmail.com>
2024-04-19 20:59:46 +0200

    added new Exception for when an InteractionTile is full

---
+++ src/game/src/main/java/org/example/ub4/excep/InteractionTileIsFullException.java
@@ -0,0 +1,7 @@
+package org.example.ub4.excep;
+
+public class InteractionTileIsFullException extends Exception {
+    public InteractionTileIsFullException(String message) {
+        super(message);
+    }
+}
