442cf85c12588ef0d63534df242f5f2be2a152e6
Saveli <saveli.gulas@gmail.com>
2024-04-19 20:59:30 +0200

    added new abstract method to add player to an InteractionTile

--- src/game/src/main/java/org/example/ub4/tile/InteractionTile.java
+++ src/game/src/main/java/org/example/ub4/tile/InteractionTile.java
@@ -1,5 +1,6 @@
 package org.example.ub4.tile;
 
+import org.example.ub4.excep.InteractionTileIsFullException;
 import org.example.ub4.interactions.Interaction;
 import org.example.ub4.interactions.InteractionResult;
 import org.example.ub4.player.Player;
@@ -17,6 +18,8 @@ public abstract class InteractionTile extends Tile {
     public InteractionTile(int id, Tile north, Tile east, Tile south, Tile west) {
         super(id, north, east, south, west);
     }
+
+    public abstract void addPlayerToTile(Player player) throws InteractionTileIsFullException;
 
     public abstract InteractionResult interactOnTile(Player player, Interaction interaction);
 }
