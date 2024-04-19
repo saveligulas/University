fa6c482f0de04608fe95c6c9532a9a86ebc73d84
Saveli <saveli.gulas@gmail.com>
2024-04-19 16:12:15 +0200

    replaced constructors with specialized constructor

--- src/game/src/main/java/org/example/ub4/tile/passthrough/HorizontalTile.java
+++ src/game/src/main/java/org/example/ub4/tile/passthrough/HorizontalTile.java
@@ -1,18 +1,15 @@
 package org.example.ub4.tile.passthrough;
 
+import org.example.ub4.tile.Direction;
 import org.example.ub4.tile.PassThroughTile;
 import org.example.ub4.tile.Tile;
 
 public abstract class HorizontalTile extends PassThroughTile {
-    public HorizontalTile(int id, Tile north, Tile east, Tile south, Tile west) {
-        super(id, north, east, south, west);
-    }
-
-    public HorizontalTile(int id, Tile[] neighbours) {
-        super(id, neighbours);
-    }
-
     public HorizontalTile(int id) {
         super(id);
+    }
+
+    public HorizontalTile(int id, Direction sourceDirection, Tile source, Direction destinationDirection, Tile destination) {
+        super(id, sourceDirection, source, destinationDirection, destination);
     }
 }
