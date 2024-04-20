989e3ada60faa3b50ea7a9f641f2736b0b9b40a5
saveligulas <saveli.gulas@gmail.com>
2024-04-20 17:07:13 +0200

    added new Factory, that creates the interaction results for all Tiles

---
+++ src/game/src/main/java/org/example/ub4/interactions/TileInteractionResultFactory.java
@@ -0,0 +1,10 @@
+package org.example.ub4.interactions;
+
+import org.example.ub4.player.Player;
+import org.example.ub4.tile.Tile;
+
+public class TileInteractionResultFactory {
+    public static InteractionResult createResult(Tile sourceTile, Tile targetTile, Player player, Interaction interaction) {
+        return null;
+    }
+}
