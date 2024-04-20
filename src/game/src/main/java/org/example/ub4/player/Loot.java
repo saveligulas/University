f3881899d6ef8a9eda731bc3529f88a1dc0ffbc3
saveligulas <saveli.gulas@gmail.com>
2024-04-20 17:36:12 +0200

    added new Superclass for all items

---
+++ src/game/src/main/java/org/example/ub4/player/Loot.java
@@ -0,0 +1,27 @@
+package org.example.ub4.player;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Loot {
+    private static final AtomicInteger ID_COUNTER = new AtomicInteger();
+    private final int _id;
+    private String _description;
+
+    public Loot(String description) {
+        _id = ID_COUNTER.getAndIncrement();
+        _description = description;
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    public String getDescription() {
+        return _description;
+    }
+
+    protected void setDescription(String description) {
+        _description = description;
+    }
+}
+
