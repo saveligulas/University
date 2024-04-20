9002b746adf0fa7b3d374cc75c4607545b50ecd1
saveligulas <saveli.gulas@gmail.com>
2024-04-20 17:08:07 +0200

    added new DoorTile

---
+++ src/game/src/main/java/org/example/ub4/tile/passthrough/DoorTile.java
@@ -0,0 +1,26 @@
+package org.example.ub4.tile.passthrough;
+
+import org.example.ub4.interactions.Interaction;
+import org.example.ub4.interactions.InteractionResult;
+import org.example.ub4.player.Player;
+import org.example.ub4.tile.Direction;
+import org.example.ub4.tile.Tile;
+
+import java.util.List;
+
+public class DoorTile extends HorizontalTile {
+    public DoorTile(int id) {
+        super(id);
+    }
+
+
+
+    public DoorTile(int id, Direction sourceDirection, Tile source, Direction destinationDirection, Tile destination) {
+        super(id, sourceDirection, source, destinationDirection, destination);
+    }
+
+    @Override
+    public void passThrough(Player player) {
+
+    }
+}
