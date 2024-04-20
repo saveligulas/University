a3df93926cdb15bfff69658fc8dd8fe29dce1682
Saveli <saveli.gulas@gmail.com>
2024-04-20 22:52:45 +0200

    specified Type of Tile in constructor

--- src/game/src/main/java/org/example/ub4/tile/passthrough/HorizontalTile.java
+++ src/game/src/main/java/org/example/ub4/tile/passthrough/HorizontalTile.java
@@ -1,6 +1,7 @@
 package org.example.ub4.tile.passthrough;
 
 import org.example.ub4.tile.Direction;
+import org.example.ub4.tile.InteractionTile;
 import org.example.ub4.tile.PassThroughTile;
 import org.example.ub4.tile.Tile;
 
@@ -9,7 +10,7 @@ public abstract class HorizontalTile extends PassThroughTile {
         super(id);
     }
 
-    public HorizontalTile(int id, Direction sourceDirection, Tile source, Direction destinationDirection, Tile destination) {
+    public HorizontalTile(int id, Direction sourceDirection, InteractionTile source, Direction destinationDirection, InteractionTile destination) {
         super(id, sourceDirection, source, destinationDirection, destination);
     }
 }
