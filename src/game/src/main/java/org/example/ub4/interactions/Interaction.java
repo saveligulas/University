e90917dec7c13fe02a0cb3aa6e7e1cf7390d755f
Saveli <saveli.gulas@gmail.com>
2024-04-20 22:52:54 +0200

    removed Legacy Code

--- src/game/src/main/java/org/example/ub4/interactions/Interaction.java
+++
@@ -1,5 +0,0 @@
-package org.example.ub4.interactions;
-
-public enum Interaction { //TODO: move and split to NeighbourInteraction and OnTileInteraction
-    NONE, DISCOVER, ADD, OPEN, CLOSE, PASS_THROUGH
-}
