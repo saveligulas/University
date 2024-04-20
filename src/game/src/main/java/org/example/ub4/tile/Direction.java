6ad4dd04404600ec1e1e39a9888ff4a62f565653
Saveli <saveli.gulas@gmail.com>
2024-04-20 22:52:00 +0200

    added static method to get opposite Direction

--- src/game/src/main/java/org/example/ub4/tile/Direction.java
+++ src/game/src/main/java/org/example/ub4/tile/Direction.java
@@ -1,5 +1,15 @@
 package org.example.ub4.tile;
 
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST
+    NORTH, EAST, SOUTH, WEST;
+
+    public static Direction getOppositeDirection(Direction direction) {
+        return switch (direction) {
+            case NORTH -> SOUTH;
+            case EAST -> WEST;
+            case SOUTH -> NORTH;
+            case WEST -> EAST;
+            default -> throw new IllegalArgumentException();
+        };
+    }
 }
