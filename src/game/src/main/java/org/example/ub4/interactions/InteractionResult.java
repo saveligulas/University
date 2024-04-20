e452e98f941234fdab3a82395cf3b761cd253725
saveligulas <saveli.gulas@gmail.com>
2024-04-20 17:08:41 +0200

    modified InteractionResult to be immutable

--- src/game/src/main/java/org/example/ub4/interactions/InteractionResult.java
+++ src/game/src/main/java/org/example/ub4/interactions/InteractionResult.java
@@ -4,17 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InteractionResult {
-    private boolean _success;
-    private String _message;
-    private List<Interaction> _possibleNextInteractions;
+    private final boolean _success;
+    private final String _message;
+    private final List<Interaction> _possibleNextInteractions;
 
-    public InteractionResult(String message) {
-        _success = false;
-        _message = message;
-        _possibleNextInteractions = new ArrayList<>();
+    public InteractionResult() {
+        this("Empty");
     }
 
-    public static InteractionResult empty(String message) {
+    public InteractionResult(String message) {
+        this(false, message, new ArrayList<>());
+    }
+
+    public InteractionResult(boolean success, String message, List<Interaction> possibleNextInteractions) {
+        _success = success;
+        _message = message;
+        _possibleNextInteractions = possibleNextInteractions;
+    }
+
+    public static InteractionResult emptyUnsuccessful(String message) {
         return new InteractionResult(message);
+    }
+
+    public static class Builder {
+        private boolean _success;
+        private String _message;
+        private final List<Interaction> _possibleNextInteractions;
+
+        public Builder() {
+            _possibleNextInteractions = new ArrayList<>();
+        }
+
+        public Builder success(boolean success) {
+            _success = success;
+            return this;
+        }
+
+        public Builder message(String message) {
+            _message = message;
+            return this;
+        }
+
+        public Builder addPossibleNextInteraction(Interaction interaction) {
+            _possibleNextInteractions.add(interaction);
+            return this;
+        }
+
+        public InteractionResult build() {
+            return new InteractionResult(_success, _message, new ArrayList<>(_possibleNextInteractions));
+        }
     }
 }
