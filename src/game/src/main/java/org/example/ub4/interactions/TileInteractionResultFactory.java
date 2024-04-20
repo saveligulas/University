d7c6a4eb6c950a7435ed66ffff66587b5ac729e8
saveligulas <saveli.gulas@gmail.com>
2024-04-20 17:40:34 +0200

    added method for both types of InteractionResults

--- src/game/src/main/java/org/example/ub4/interactions/TileInteractionResultFactory.java
+++ src/game/src/main/java/org/example/ub4/interactions/TileInteractionResultFactory.java
@@ -4,7 +4,11 @@ import org.example.ub4.player.Player;
 import org.example.ub4.tile.Tile;
 
 public class TileInteractionResultFactory {
-    public static InteractionResult createResult(Tile sourceTile, Tile targetTile, Player player, Interaction interaction) {
+    public static InteractionResult<OnTileInteraction> createResult(Tile sourceTile, Tile targetTile, Player player, OnTileInteraction interaction) {
+        return null;
+    }
+
+    public static InteractionResult<NeighbourTileInteraction> createResult(Tile sourceTile, Tile targetTile, Player player, NeighbourTileInteraction interaction) {
         return null;
     }
 }
