310ca8ec8114a8aa3a05219da4d93f3ba8fe8699
saveligulas <saveli.gulas@gmail.com>
2024-04-21 23:34:02 +0200

    changed visibility of methods

--- src/game/src/main/java/org/example/ub4/game/Game.java
+++ src/game/src/main/java/org/example/ub4/game/Game.java
@@ -1,12 +1,18 @@
 package org.example.ub4.game;
 
-import org.example.ub4.interactions.InteractionResult;
+import org.example.ub4.excep.TileCanNotBeAccessedException;
+import org.example.ub4.interactions.*;
 import org.example.ub4.player.Player;
+import org.example.ub4.tile.Tile;
 
 public class Game {
     private GameState _state;
 
-    private void checkIfPlayerIsInGame(Player player) {
+    Game(GameState state) {
+        _state = state;
+    }
+
+    protected void checkIfPlayerIsInGame(Player player) {
         if (!_state.hasPlayer(player)) {
             throw new RuntimeException("Player is not in game");
         }
@@ -18,15 +24,34 @@ public class Game {
     }
 
     // gives us the options the player has for this turn
-    public PlayerOptions getPlayerOptions() {
-        Player player = _state.getCurrentPlayer();
-
+    protected OnTileInteractionResult getOnTileOptions() {
+        return _state.getTileOfCurrentPlayer().interactOnTile(_state.getCurrentPlayer());
 
     }
 
-    // gives us the result of the interaction and if it worked
-    public <T extends Enum<T>> InteractionResult<T> performAction(T interaction) {
+    protected NeighbourTileInteractionResult getNeighbouringTileOptions(Tile neighbourTile) throws TileCanNotBeAccessedException {
+        return _state.getTileOfCurrentPlayer().interactWithNeighbouringTile(neighbourTile, _state.getCurrentPlayer());
+    }
 
+    // gives us the result of the interaction and if it worked
+    protected OnTileInteractionResult performAction(OnTileInteraction onTileInteraction) {
+        return _state.getTileOfCurrentPlayer().interactOnTile(_state.getCurrentPlayer(), onTileInteraction);
+    }
+
+    protected NeighbourTileInteractionResult performAction(Tile neighbourTile, NeighbourTileInteraction neighbourTileInteraction) {
+        try {
+            return _state.getTileOfCurrentPlayer().interactWithNeighbouringTile(neighbourTile, _state.getCurrentPlayer(), neighbourTileInteraction);
+        } catch (TileCanNotBeAccessedException e) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Unexpected error occurred while trying to interact with a Tile that is not a neighbour.\n");
+            sb.append("Cause: ").append(e.getCause().getMessage()).append("\n");
+            sb.append("Message: ").append(e.getMessage());
+
+            NeighbourTileInteractionResult.Builder builder = new NeighbourTileInteractionResult.Builder();
+            return builder.success(false)
+                    .message(sb.toString())
+                    .build();
+        }
     }
 
 }
