d42d9d02b305cddc73b2aececb92a7d4e1b5cc52
saveligulas <saveli.gulas@gmail.com>
2024-04-21 13:09:51 +0200

    added new classes for game logic and TODO

---
+++ src/game/src/main/java/org/example/ub4/game/PlayerTurn.java
@@ -0,0 +1,4 @@
+package org.example.ub4.game;
+
+public class PlayerTurn {
+}
---
+++ src/game/src/main/java/org/example/ub4/game/RoundInfo.java
@@ -0,0 +1,8 @@
+package org.example.ub4.game;
+
+public class RoundInfo {
+    //TODO: implement
+    public static RoundInfo buildRoundInfo(GameState state) {
+
+    }
+}
