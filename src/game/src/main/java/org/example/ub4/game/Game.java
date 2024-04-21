8daa3ea7247e2c990529d67b5a441d97c46d1e28
saveligulas <saveli.gulas@gmail.com>
2024-04-21 13:08:49 +0200

    added basic structure of game

--- src/game/src/main/java/org/example/ub4/game/Game.java
+++ src/game/src/main/java/org/example/ub4/game/Game.java
@@ -1,4 +1,32 @@
 package org.example.ub4.game;
 
+import org.example.ub4.interactions.InteractionResult;
+import org.example.ub4.player.Player;
+
 public class Game {
+    private GameState _state;
+
+    private void checkIfPlayerIsInGame(Player player) {
+        if (!_state.hasPlayer(player)) {
+            throw new RuntimeException("Player is not in game");
+        }
+    }
+
+    // gives us the info of what round we are on and which players are still in the game
+    public RoundInfo getRoundInfo() {
+        return RoundInfo.buildRoundInfo(_state);
+    }
+
+    // gives us the options the player has for this turn
+    public PlayerOptions getPlayerOptions() {
+        Player player = _state.getCurrentPlayer();
+
+
+    }
+
+    // gives us the result of the interaction and if it worked
+    public <T extends Enum<T>> InteractionResult<T> performAction(T interaction) {
+
+    }
+
 }
