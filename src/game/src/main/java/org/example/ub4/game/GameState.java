61baf0b6f152ad8fc20d6ad8eee1d46a7ec57c50
saveligulas <saveli.gulas@gmail.com>
2024-04-21 13:09:18 +0200

    added and finished Data class for GameState

---
+++ src/game/src/main/java/org/example/ub4/game/GameState.java
@@ -0,0 +1,51 @@
+package org.example.ub4.game;
+
+import org.example.ub4.player.Player;
+
+import java.util.List;
+
+public class GameState {
+    private int _currentRound;
+    private final int _maxRound;
+    private final List<Player> _players;
+    private int _playerTurnPointer;
+
+    public GameState(int roundsToPlay, Player... players) {
+        this(roundsToPlay, List.of(players));
+    }
+
+    public GameState(int roundsToPlay, List<Player> players) {
+        _currentRound = 1;
+        _maxRound = roundsToPlay;
+        _players = players;
+    }
+
+    public void advancePlayerTurn() {
+        _playerTurnPointer++;
+    }
+
+    public void advanceRound() {
+        _currentRound++;
+        _playerTurnPointer = 0;
+    }
+
+    public boolean isOver() {
+        return _currentRound == _maxRound;
+    }
+
+    public boolean hasPlayer(Player player) {
+        return _players.contains(player);
+    }
+
+    public boolean removePlayer(Player player) {
+        return _players.remove(player);
+    }
+
+    public Player getCurrentPlayer() {
+        return _players.get(_playerTurnPointer);
+    }
+
+    public List<Player> getPlayers() {
+        return _players;
+    }
+}
