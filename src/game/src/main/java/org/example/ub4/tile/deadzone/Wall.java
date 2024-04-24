731ca1d859b7de19e736f92b0bc8863e9f63e853
Saveli <saveli.gulas@gmail.com>
2024-04-24 22:42:31 +0200

    implemented methods

--- src/game/src/main/java/org/example/ub4/tile/deadzone/Wall.java
+++ src/game/src/main/java/org/example/ub4/tile/deadzone/Wall.java
@@ -1,9 +1,16 @@
 package org.example.ub4.tile.deadzone;
 
+import org.example.ub4.interactions.NeighbourTileInteraction;
+import org.example.ub4.interactions.NeighbourTileInteractionResult;
+import org.example.ub4.player.Player;
 import org.example.ub4.tile.DeadZoneTile;
 import org.example.ub4.tile.Tile;
 
+import java.util.List;
+
 public class Wall extends DeadZoneTile {
+    public Wall() {super();};
+
     public Wall(int id) {
         super(id);
     }
@@ -19,5 +26,15 @@ public class Wall extends DeadZoneTile {
     @Override
     protected void setDescription() {
         _description = "This is a wall.";
+    }
+
+    @Override
+    protected void setSpecification() {
+        _specification = "Wall";
+    }
+
+    @Override
+    protected List<NeighbourTileInteraction> addMorePossibleInteractions() {
+        return List.of();
     }
 }
