d88292735113568035087ebc982eed882cb815a4
Saveli <saveli.gulas@gmail.com>
2024-04-24 22:44:39 +0200

    commented out legacy code

--- src/game/src/main/java/org/example/ub4/interactions/TileInteractionResultManager.java
+++ src/game/src/main/java/org/example/ub4/interactions/TileInteractionResultManager.java
@@ -10,40 +10,40 @@ import java.util.List;
 
 //TODO: Deprecate entire Legacy class and move implementation directly into Tiles
 public class TileInteractionResultManager {
-    public static OnTileInteractionResult createResult(Player player, OnTileInteraction interaction) {
-        if (interaction == OnTileInteraction.PASS) {
-            OnTileInteractionResult.Builder builder = new OnTileInteractionResult.Builder();
-            builder.success(true);
-            builder.message("You have passed your interaction turn.");
-            return builder.build();
-        }
-
-        if (interaction == null) {
-            return new OnTileInteractionResult(true, "These are the actions you can perform on this tile:", player.getTile().getOnTileInitialInteractions());
-        }
-
-        return player.getTile().interactOnTile(player, interaction);
-    }
-
-
-    public static NeighbourTileInteractionResult createResult(Tile targetTile, Player player, NeighbourTileInteraction interaction) throws TileCanNotBeAccessedException {
-        if (interaction == NeighbourTileInteraction.PASS) {
-            NeighbourTileInteractionResult.Builder builder = new NeighbourTileInteractionResult.Builder();
-            builder.success(true);
-            builder.message("You have passed your interaction turn.");
-            return builder.build();
-        }
-
-        if (interaction == null) {
-            return createOptionsNeighbour(targetTile);
-        }
-
-        return player.getTile().interactWithNeighbouringTile(targetTile, player, interaction);
-    }
-
-    private static NeighbourTileInteractionResult createOptionsNeighbour(Tile targetTile) {
-        List<NeighbourTileInteraction> result = targetTile.getPossibleInteractions();
-        result.add(NeighbourTileInteraction.PASS);
-        return new NeighbourTileInteractionResult(true, "These are the options you have for this tile", result);
-    }
+//    public static OnTileInteractionResult createResult(Player player, OnTileInteraction interaction) {
+//        if (interaction == OnTileInteraction.PASS) {
+//            OnTileInteractionResult.Builder builder = new OnTileInteractionResult.Builder();
+//            builder.success(true);
+//            builder.message("You have passed your interaction turn.");
+//            return builder.build();
+//        }
+//
+//        if (interaction == null) {
+//            return new OnTileInteractionResult(true, "These are the actions you can perform on this tile:", player.getTile().getOnTileInitialInteractions());
+//        }
+//
+//        return player.getTile().interactOnTile(player, interaction);
+//    }
+//
+//
+//    public static NeighbourTileInteractionResult createResult(Tile targetTile, Player player, NeighbourTileInteraction interaction) throws TileCanNotBeAccessedException {
+//        if (interaction == NeighbourTileInteraction.PASS) {
+//            NeighbourTileInteractionResult.Builder builder = new NeighbourTileInteractionResult.Builder();
+//            builder.success(true);
+//            builder.message("You have passed your interaction turn.");
+//            return builder.build();
+//        }
+//
+//        if (interaction == null) {
+//            return createOptionsNeighbour(targetTile);
+//        }
+//
+//        return player.getTile().interactWithNeighbouringTile(targetTile, player, interaction);
+//    }
+//
+//    private static NeighbourTileInteractionResult createOptionsNeighbour(Tile targetTile) {
+//        List<NeighbourTileInteraction> result = targetTile.getPossibleInteractions();
+//        result.add(NeighbourTileInteraction.PASS);
+//        return new NeighbourTileInteractionResult(true, "These are the options you have for this tile", result);
+//    }
 }
