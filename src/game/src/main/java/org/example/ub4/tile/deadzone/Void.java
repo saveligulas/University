10cb8ba459e1c0a3e6543377e9ebfca4118e99e3
Saveli <saveli.gulas@gmail.com>
2024-04-24 22:42:36 +0200

    implemented methods

--- src/game/src/main/java/org/example/ub4/tile/deadzone/Void.java
+++ src/game/src/main/java/org/example/ub4/tile/deadzone/Void.java
@@ -1,29 +1,35 @@
 package org.example.ub4.tile.deadzone;
 
+import org.example.ub4.interactions.NeighbourTileInteraction;
+import org.example.ub4.interactions.NeighbourTileInteractionResult;
+import org.example.ub4.player.Player;
 import org.example.ub4.tile.DeadZoneTile;
 import org.example.ub4.tile.Direction;
 import org.example.ub4.tile.Tile;
 
-public class Void extends DeadZoneTile {
-    public Void(int id) {
-        super(id);
-    }
+import java.util.List;
 
-    public Void(int id, Direction direction, Tile edgeTile) {
-        super(id);
-        setTile(direction, edgeTile);
+public class Void extends DeadZoneTile {
+    public Void() {
+        super();
     }
 
     public Void(int id, Tile[] neighbours) {
         super(id, neighbours);
     }
 
-    public Void(int id, Tile north, Tile east, Tile south, Tile west) {
-        super(id, north, east, south, west);
+    @Override
+    protected void setDescription() {
+        _description = "The Void is neither here nor there. It swallows anything it comes into contact with.";
     }
 
     @Override
-    protected void setDescription() {
-        _description = "This is the void.";
+    protected void setSpecification() {
+        _specification = "Void";
+    }
+
+    @Override
+    protected List<NeighbourTileInteraction> addMorePossibleInteractions() {
+        return List.of();
     }
 }
