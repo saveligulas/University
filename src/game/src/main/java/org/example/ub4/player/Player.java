1b485c77df0f74961a8827f2bca532f2084ae347
Saveli <saveli.gulas@gmail.com>
2024-04-24 14:25:37 +0200

    added toString

--- src/game/src/main/java/org/example/ub4/player/Player.java
+++ src/game/src/main/java/org/example/ub4/player/Player.java
@@ -1,23 +1,26 @@
 package org.example.ub4.player;
 
 import org.example.ub4.excep.InteractionTileIsFullException;
+import org.example.ub4.tile.Direction;
 import org.example.ub4.tile.InteractionTile;
+import org.example.ub4.tile.Tile;
+import org.example.ub4.tile.deadzone.Void;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Player {
     private final String _username;
     private final List<Loot> _inventory;
+    //TODO: implement this
     private final List<String> _answers;
     private InteractionTile _interactionTile;
-    private final List<Integer> _discoveredTiles;
+    private final Set<Integer> _discoveredTiles;
 
     public Player(String username) {
         _username = username;
         _inventory = new ArrayList<>();
         _answers = new ArrayList<>();
-        _discoveredTiles = new ArrayList<>();
+        _discoveredTiles = new HashSet<>();
     }
 
     public boolean setTile(InteractionTile tile) {
@@ -32,11 +35,16 @@ public class Player {
         }
 
         _interactionTile = tile;
+        _discoveredTiles.add(tile.getId());
         return true;
     }
 
     public void addLoot(List<Loot> loot) {
         _inventory.addAll(loot);
+    }
+
+    public void addDiscoveredTile(Tile tile) {
+        _discoveredTiles.add(tile.getId());
     }
 
     public InteractionTile getTile() {
@@ -45,5 +53,33 @@ public class Player {
 
     public String getUsername() {
         return _username;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(_username).append("\n");
+        sb.append("Inventory:\n");
+        for (Loot loot : _inventory) {
+            sb.append(loot).append("\n");
+        }
+        sb.append("Tiles:\n");
+        sb.append("Current - ").append(_interactionTile.shortToString()).append("\n");
+        for (Direction direction : Direction.values()) {
+            sb.append(direction.toString().charAt(0)).append(": ");
+            Optional<Tile> optionalTile = _interactionTile.getTileInDirection(direction);
+            if (optionalTile.isPresent()) {
+                Tile directionTile = optionalTile.get();
+                if (directionTile instanceof Void v) {
+                    sb.append(v.shortToString());
+                } else if(_discoveredTiles.contains(directionTile.getId())) {
+                    sb.append(directionTile.shortToString());
+                }
+            } else {
+                sb.append("Unknown");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
