1cde69ab946efc2e8732a39099e0a38d6cfd1894
Saveli <saveli.gulas@gmail.com>
2024-04-24 22:48:07 +0200

    expanded methods

--- src/game/src/main/java/org/example/ub4/game/GameInitializer.java
+++ src/game/src/main/java/org/example/ub4/game/GameInitializer.java
@@ -1,22 +1,26 @@
 package org.example.ub4.game;
 
 import org.example.ub4.player.Player;
+import org.example.ub4.tile.InteractionTile;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class GameInitializer {
-    public static Game initialize(int roundsToPlay, String... usernames) {
-        return initialize(roundsToPlay, Set.copyOf(List.of(usernames)));
+    public static Game initialize(int roundsToPlay, InteractionTile sourceTile,  String... usernames) {
+        return initialize(roundsToPlay, sourceTile, Set.copyOf(List.of(usernames)));
     }
 
-    public static Game initialize(int roundsToPlay, Set<String> usernames) {
+    public static Game initialize(int roundsToPlay, InteractionTile sourceTile, Set<String> usernames) {
         List<Player> players = new ArrayList<>();
         for (String username : usernames) {
             players.add(new Player(username));
         }
-        GameState state = new GameState(roundsToPlay, players);
+        for (Player player : players) {
+            player.setTile(sourceTile);
+        }
+        GameState state = new GameState(roundsToPlay, sourceTile, players);
         return new Game(state);
     }
 }
