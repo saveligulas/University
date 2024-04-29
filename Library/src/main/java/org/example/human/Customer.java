779f274c98a427c569de16a225a466dc9e6d1b29
saveligulas <saveli.gulas@gmail.com>
2024-04-29 10:12:31 +0200

    .

--- Library/src/main/java/org/example/human/Customer.java
+++ Library/src/main/java/org/example/human/Customer.java
@@ -1,17 +1,15 @@
 package org.example.human;
 
-import org.example.obj.NameOfPerson;
-
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class Lender {
+public class Customer {
     private static final AtomicInteger ID_COUNTER = new AtomicInteger();
 
     private final int _id;
     private final NameOfPerson _name;
     private LendingProfile _profile;
 
-    public Lender(NameOfPerson name, LendingProfile profile) {
+    public Customer(NameOfPerson name, LendingProfile profile) {
         _id = ID_COUNTER.getAndIncrement();
         this._name = name;
         _profile = profile;
