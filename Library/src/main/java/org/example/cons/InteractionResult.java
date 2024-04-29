3c4ea56bd453acefa6eea6ca46bb520c509bafc0
Saveli <saveli.gulas@gmail.com>
2024-04-29 11:22:18 +0200

    .

--- Library/src/main/java/org/example/cons/InteractionResult.java
+++ Library/src/main/java/org/example/cons/InteractionResult.java
@@ -1,4 +1,13 @@
 package org.example.cons;
 
+import java.util.List;
+
 public class InteractionResult {
+    private final boolean _success;
+    private final String _message;
+
+    public InteractionResult(boolean success, String message) {
+        _success = success;
+        _message = message;
+    }
 }
