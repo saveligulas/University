c60a53a31c05f3102ceb6f7684c2cf673d43d3a3
saveligulas <saveli.gulas@gmail.com>
2024-04-29 19:11:27 +0200

    .

--- Library/src/main/java/org/example/cons/InteractionResult.java
+++ Library/src/main/java/org/example/cons/InteractionResult.java
@@ -1,13 +1,23 @@
 package org.example.cons;
 
+import org.example.lib.LibraryItem;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class InteractionResult {
     private final boolean _success;
     private final String _message;
+    private final List<LibraryItem> _items;
 
     public InteractionResult(boolean success, String message) {
         _success = success;
         _message = message;
+        _items = new ArrayList<>();
+    }
+
+    public InteractionResult addItems(List<LibraryItem> items) {
+        _items.addAll(items);
+        return this;
     }
 }
