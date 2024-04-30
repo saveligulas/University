42d1d51f76707bf3ece72cd641c737ad31f50408
Saveli <saveli.gulas@gmail.com>
2024-05-01 01:29:19 +0200

    .

---
+++ Library/src/main/java/org/example/obj/TupleBidirectional.java
@@ -0,0 +1,15 @@
+package org.example.obj;
+
+public class TupleBidirectional<K> extends Tuple<K, K> {
+
+    public TupleBidirectional(K k1, K k2) {
+        super(k1, k2);
+    }
+
+    public boolean equalsOneValue(K k) {
+        if (getFirst() == null || getSecond() == null) {
+            return false;
+        }
+        return getFirst().equals(k) || getSecond().equals(k);
+    }
+}
