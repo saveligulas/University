c4bb210592c421fb45bc996db75a86bee4160753
Saveli <saveli.gulas@gmail.com>
2024-05-01 01:28:56 +0200

    .

--- Library/src/main/java/org/example/lib/LibraryItem.java
+++ Library/src/main/java/org/example/lib/LibraryItem.java
@@ -1,7 +1,8 @@
 package org.example.lib;
 
 import org.example.exc.ItemIsNotAvailableException;
-import org.example.exc.ReservationQueueIsEmptyException;
+import org.example.exc.ReservingItemCustomerHasAlreadyLentException;
+import org.example.exc.ReservingItemMultipleTimesConsecutivelyException;
 import org.example.human.Customer;
 import org.example.human.NameOfPerson;
 import org.example.obj.MyPriorityQueue;
@@ -15,47 +16,49 @@ public abstract class LibraryItem {
     private final String _identifier;
     private final String _publisher;
     private final NameOfPerson _intellectualOwner;
+    private final Category _primaryCategory;
+    private final List<Category> _themeCategories;
     //Must ensure there is always one LendingInfo inside
     MyPriorityQueue<Reservation> _reservations;
 
 
-    public LibraryItem(int copyNumber, String title, String publisher, NameOfPerson intellectualOwner, String identifier) {
+    public LibraryItem(int copyNumber, String title, String publisher, NameOfPerson intellectualOwner, String identifier, Category primaryCategory, Category... themeCategories) {
         _copyNumber = copyNumber;
         _title = title;
         _publisher = publisher;
         _intellectualOwner = intellectualOwner;
         _identifier = identifier;
-        _reservations = new MyPriorityQueue<>(List.of(new Reservation()));
+        _reservations = new MyPriorityQueue<>();
+        _primaryCategory = primaryCategory;
+        _themeCategories = List.of(themeCategories);
     }
 
     public abstract int getWeeksCustomerCanLendFor(Customer customer);
 
     public abstract int getMaxExtensions(Customer customer);
 
+    //TODO: add exceptions when a customer wants to reserve a book multiple times in a row
     public Reservation lend(Customer customer, int weeks) {
-        if (_reservations.isEmpty()) {
-            throw new ReservationQueueIsEmptyException();
-        }
-        if (_reservations.peek().isActive()) {
+        if (_reservations.size() > 0 && _reservations.peek().isActive()) {
             throw new ItemIsNotAvailableException();
         }
         LocalDate now = LocalDate.now();
         int weeksMax = getWeeksCustomerCanLendFor(customer);
         LocalDate endDate = weeks <= 0 || weeks > weeksMax ? now.plusWeeks(weeksMax) : now.plusWeeks(weeks);
-        return addReservation(customer, now, endDate, true);
+        return addReservationToQueue(customer, now, endDate, true);
     }
 
-    public Reservation reserve(Customer customer, int weeks, LocalDate startDate) {
-        if (_reservations.size() == 1) {
-            return _reservations.peek();
-        }
-        LocalDate start = startDate.equals(LocalDate.MAX) ? _reservations.peekLast().getEndDate().plusWeeks(weeks) : startDate;
+    public Reservation reserve(Customer customer, int weeks) throws ReservingItemCustomerHasAlreadyLentException, ReservingItemMultipleTimesConsecutivelyException {
+        return reserve(customer, weeks, getEarliestDateAvailable());
+    }
+
+    private Reservation reserve(Customer customer, int weeks, LocalDate startDate) {
         int weeksMax = getWeeksCustomerCanLendFor(customer);
-        LocalDate endDate = weeks <= 0 || weeks > weeksMax ? start.plusWeeks(weeksMax) : start.plusWeeks(weeks);
-        return addReservation(customer, startDate, endDate, false);
+        LocalDate endDate = weeks <= 0 || weeks > weeksMax ? startDate.plusWeeks(weeksMax) : startDate.plusWeeks(weeks);
+        return addReservationToQueue(customer, startDate, endDate, false);
     }
 
-    private Reservation addReservation(Customer customer, LocalDate startDate, LocalDate endDate, boolean isActive) {
+    private Reservation addReservationToQueue(Customer customer, LocalDate startDate, LocalDate endDate, boolean isActive) {
         Reservation reservation = new Reservation(customer.getId(), isActive, startDate, endDate, getMaxExtensions(customer));
         _reservations.add(reservation);
         return reservation;
@@ -63,7 +66,7 @@ public abstract class LibraryItem {
 
     public boolean hasActiveLender() {
         if (_reservations.isEmpty()) {
-            throw new ReservationQueueIsEmptyException();
+            return false;
         }
         return _reservations.peek().isActive();
     }
