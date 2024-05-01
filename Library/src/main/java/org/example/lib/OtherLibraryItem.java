0a00e3d829707d5a924911066369d5e6ad4f79ff
Saveli <saveli.gulas@gmail.com>
2024-05-02 00:06:45 +0200

    .

--- Library/src/main/java/org/example/lib/OtherLibraryItem.java
+++ Library/src/main/java/org/example/lib/OtherLibraryItem.java
@@ -3,10 +3,13 @@ package org.example.lib;
 import org.example.human.Customer;
 import org.example.human.NameOfPerson;
 
+import java.util.List;
+
 public abstract class OtherLibraryItem extends LibraryItem {
 
-    public OtherLibraryItem(int copyNumber, String title, String publisher, NameOfPerson intellectualOwner, String identifier, Category primaryCategory, Category... themeCategories) {
-        super(copyNumber, title, publisher, intellectualOwner, identifier, primaryCategory, themeCategories);
+
+    public OtherLibraryItem(int copyNumber, String title, String publisher, NameOfPerson intellectualOwner, String identifier, Section section, List<Category> themeCategories) {
+        super(copyNumber, title, publisher, intellectualOwner, identifier, section, themeCategories);
     }
 
     @Override
