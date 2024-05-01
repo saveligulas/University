8e4a1d345cd9fe5d890c5740b221f64527ddca87
Saveli <saveli.gulas@gmail.com>
2024-05-02 00:06:16 +0200

    .

--- Library/src/main/java/org/example/cons/InteractionResult.java
+++ Library/src/main/java/org/example/cons/InteractionResult.java
@@ -8,16 +8,13 @@ import java.util.List;
 public class InteractionResult {
     private final boolean _success;
     private final String _message;
-    private final List<LibraryItem> _items;
 
     public InteractionResult(boolean success, String message) {
         _success = success;
         _message = message;
-        _items = new ArrayList<>();
     }
 
-    public InteractionResult addItems(List<LibraryItem> items) {
-        _items.addAll(items);
-        return this;
+    public String toString() {
+        return _success? "Success!" : "Unsuccessful!" + "\n" + _message;
     }
 }
