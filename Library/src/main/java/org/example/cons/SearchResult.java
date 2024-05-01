38be0618ddde6c185c86e141f358322ae0bb528b
Saveli <saveli.gulas@gmail.com>
2024-05-02 00:07:17 +0200

    .

--- Library/src/main/java/org/example/cons/SearchResult.java
+++ Library/src/main/java/org/example/cons/SearchResult.java
@@ -1,16 +1,27 @@
 package org.example.cons;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SearchResult {
-    private Map<String, String> _searchResults;
+    private List<SearchRecord> _searchResults;
 
     public SearchResult() {
-        _searchResults = new LinkedHashMap<>();
+        _searchResults = new ArrayList<>();
     }
 
-    public void put(String key, String status) {
-        _searchResults.put(key, status);
+    public void add(String identifier, String title, String status) {
+        _searchResults.add(new SearchRecord(identifier, title, status));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (SearchRecord r : _searchResults) {
+            sb.append(r);
+        }
+        return sb.toString();
     }
 }
