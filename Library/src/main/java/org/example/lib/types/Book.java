b05f06971d9ddeadba252bd66321efec663b09da
Saveli <saveli.gulas@gmail.com>
2024-05-02 00:06:03 +0200

    .

--- Library/src/main/java/org/example/lib/types/Book.java
+++ Library/src/main/java/org/example/lib/types/Book.java
@@ -3,11 +3,14 @@ package org.example.lib.types;
 import org.example.human.NameOfPerson;
 import org.example.lib.BookOrJournalLibraryItem;
 import org.example.lib.Category;
+import org.example.lib.Section;
+
+import java.util.List;
 
 
 public class Book extends BookOrJournalLibraryItem {
 
-    public Book(int copyNumber, String title, String publisher, NameOfPerson intellectualOwner, String identifier, Category... themeCategories) {
-        super(copyNumber, title, publisher, intellectualOwner, identifier, Category.BOOK, themeCategories);
+    public Book(int copyNumber, String title, String publisher, NameOfPerson intellectualOwner, String identifier, List<Category> themeCategories) {
+        super(copyNumber, title, publisher, intellectualOwner, identifier, Section.BOOK, themeCategories);
     }
 }
