181f00b0ba9bbfbfad969c39a11f15287bb3b6a4
Saveli <saveli.gulas@gmail.com>
2024-05-12 23:20:42 +0200

    .

--- Library/LibraryV2/src/main/java/lib/TimeService.java
+++ Library/LibraryV2/src/main/java/lib/TimeService.java
@@ -1,9 +1,16 @@
 package lib;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class TimeService {
     public static final TimeService INSTANCE = new TimeService();
 
     private TimeService() {
 
+    }
+
+    public int daysBetweenInclusive(LocalDate startDate, LocalDate endDate) {
+        return (int) ChronoUnit.DAYS.between(startDate, endDate) + 1;
     }
 }
